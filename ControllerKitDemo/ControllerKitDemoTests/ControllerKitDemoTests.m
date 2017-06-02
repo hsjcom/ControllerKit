7ab89de068c69895a024291e9a0cc354a15bfc26
Shaojie Hong <hsjcn@qq.com>
2017-06-02 16:56:00 +0800

    initial commit

---
+++ ControllerKitDemo/ControllerKitDemoTests/ControllerKitDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  ControllerKitDemoTests.m
+//  ControllerKitDemoTests
+//
+//  Created by Soldier on 2017/6/2.
+//  Copyright © 2017年 Shaojie Hong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ControllerKitDemoTests : XCTestCase
+
+@end
+
+@implementation ControllerKitDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
