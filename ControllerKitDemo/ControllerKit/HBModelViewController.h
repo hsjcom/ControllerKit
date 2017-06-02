74a492d053c5038be5dfa37c47dae23d762fbfd5
Shaojie Hong <hsjcn@qq.com>
2017-06-02 18:03:14 +0800

    ModelViewController step 1

---
+++ ControllerKitDemo/ControllerKit/HBModelViewController.h
@@ -0,0 +1,27 @@
+//
+//  HBModelViewController.h
+//  ControllerKitDemo
+//
+//  Created by Soldier on 2017/6/2.
+//  Copyright © 2017年 Shaojie Hong. All rights reserved.
+//
+
+#import "HBViewController.h"
+
+
+/**
+ 刷新类型
+
+ - PullDefault: 默认类型
+ - PullDownRefresh: 下拉刷新
+ - PullUpLoadMore: 上拉更多
+ */
+typedef NS_ENUM(NSInteger, PullLoadType) {
+    PullDefault = 0,
+    PullDownRefresh = 1,
+    PullUpLoadMore  = 2,
+};
+
+@interface HBModelViewController : HBViewController
+
+@end
---
+++ ControllerKitDemo/ControllerKit/HBModelViewController.m
@@ -0,0 +1,37 @@
+//
+//  HBModelViewController.m
+//  ControllerKitDemo
+//
+//  Created by Soldier on 2017/6/2.
+//  Copyright © 2017年 Shaojie Hong. All rights reserved.
+//
+
+#import "HBModelViewController.h"
+
+@interface HBModelViewController ()
+
+@end
+
+@implementation HBModelViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
