3faac3c87db4253edd7d9749218e4b6b26906b46
Shaojie Hong <hsjcn@qq.com>
2017-06-08 14:44:26 +0800

    上下拉刷新 step 1

--- ControllerKitDemo/ControllerKit/TableView/HBTableViewController.h
+++ ControllerKitDemo/ControllerKit/TableView/HBTableViewController.h
@@ -16,8 +16,17 @@
 @property(nonatomic, assign) PullLoadType pullLoadType;
 @property(nonatomic, assign) NSInteger newItemsCount;
 @property(nonatomic, assign) int page;
+/**
+ 能否上拉更多 default YES
+ */
 @property(nonatomic, assign) BOOL loadMoreEnable;
+/**
+ 能否下拉刷新 default YES
+ */
 @property(nonatomic, assign) BOOL loadRefreshEnable;
+
+
+#pragma mark - TableView
 
 - (UITableViewStyle)tableViewStyle;
 
@@ -27,5 +36,75 @@
  reloadData [self.tableView reloadData];
  */
 - (void)reloadData;
+
+/**
+ 点击跳转方法
+ */
+- (void)didSelectObject:(id)object atIndexPath:(NSIndexPath *)indexPath;
+
+- (void)didBeginDragging;
+
+- (void)didEndDragging;
+
+
+#pragma mark - Load
+
+/**
+ 自动下拉刷新
+ */
+- (void)autoPullDown;
+
+/**
+ 刷新数据
+ */
+- (void)refreshForNewData;
+
+/**
+ 下拉刷新
+ */
+- (void)loadNewData;
+
+/**
+ 上拉更多
+ */
+- (void)loadMoreData;
+
+/**
+ 结束下拉刷新
+ */
+- (void)endRefresh;
+
+/**
+ 结束上拉更多
+ */
+- (void)endLoadMore;
+
+/**
+ 结束上下拉状态
+ 带延时动画
+ */
+- (void)endPullStatus;
+
+/**
+ 结束上下拉状态 加载失败 to do
+ 带延时动画
+ */
+- (void)endPullStatusFail;
+
+/**
+ TableView DataSource
+ */
+- (void)createDataSource;
+
+/**
+ 处理页面是否能继续上拉更多
+ */
+- (void)handleWhenLessOnePage;
+
+/**
+ 展示空页面
+ */
+- (void)handleWhenNoneData;
+
 
 @end
--- ControllerKitDemo/ControllerKit/TableView/HBTableViewController.m
+++ ControllerKitDemo/ControllerKit/TableView/HBTableViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "HBTableViewController.h"
+#import "HBTableViewCell.h"
+#import "HBLayout.h"
+#import "MJRefresh.h"
 
 @interface HBTableViewController ()
 
@@ -25,11 +28,7 @@
     self = [super initWithQuery:query];
     if (self) {
         self.page = 1;
-        self.items = [NSMutableArray arrayWithCapacity:0];
         self.pullLoadType = PullDefault;
-        
-        _loadMoreEnable = YES;
-        _loadRefreshEnable = YES;
     }
     return self;
 }
@@ -38,6 +37,9 @@
     [super viewDidLoad];
     
     [self tableView];
+    [self configRefresh];
+    
+//    [self createModel];
 }
 
 #pragma mark - TableView
@@ -76,5 +78,234 @@
 
 #pragma mark - UITableViewDelegate
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    id <HBTableViewDataSource> dataSource = (id <HBTableViewDataSource>) tableView.dataSource;
+    id object = [dataSource tableView:tableView objectForRowAtIndexPath:indexPath];
+    
+    CGFloat height = 0;
+    /**
+     * 优化cell高度计算
+     */
+    if ([object isKindOfClass:[HBLayout class]]) {
+        HBLayout *layout = (HBLayout *)object;
+        if (layout.cellHeight > 0) {
+            return layout.cellHeight;
+        }
+        Class cls = [dataSource tableView:tableView cellClassForObject:object];
+        height = [cls tableView:tableView rowHeightForObject:object];
+        layout.cellHeight = height;
+        return height;
+        
+    } else {
+        Class cls = [dataSource tableView:tableView cellClassForObject:object];
+        height = [cls tableView:tableView rowHeightForObject:object];
+        return height;
+    }
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [_tableView deselectRowAtIndexPath:indexPath animated:NO];
+    
+    id <HBTableViewDataSource> dataSource = (id <HBTableViewDataSource>) tableView.dataSource;
+    id object = [dataSource tableView:tableView objectForRowAtIndexPath:indexPath];
+    [self didSelectObject:object atIndexPath:indexPath];
+}
+
+/**
+ 点击跳转方法
+ */
+- (void)didSelectObject:(id)object atIndexPath:(NSIndexPath *)indexPath {
+    [self.tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    [self didBeginDragging];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    [self didEndDragging];
+}
+
+- (void)didBeginDragging {
+}
+
+- (void)didEndDragging {
+}
+
+#pragma mark - 下拉刷新 & 上拉更多
+
+/**
+ 能否下拉刷新 default YES
+ */
+- (BOOL)loadRefreshEnable {
+    return YES;
+}
+
+/**
+ 能否上拉更多 default YES
+ */
+- (BOOL)loadMoreEnable {
+    return YES;
+}
+
+- (void)configRefresh {
+    __weak __typeof(self) weakSelf = self;
+    
+    //下拉刷新
+    if (self.loadRefreshEnable) {
+        self.tableView.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+            [weakSelf loadNewData];
+        }];
+    }
+    
+    if (self.loadMoreEnable) {
+        //上拉更多
+        MJRefreshAutoNormalFooter *footer = [MJRefreshAutoNormalFooter footerWithRefreshingBlock:^{
+            [weakSelf loadMoreData];
+        }];
+        
+        [footer setTitle:@"Click or drag up to refresh" forState:MJRefreshStateIdle];
+        [footer setTitle:@"Loading more ..." forState:MJRefreshStateRefreshing];
+        [footer setTitle:@"- END -" forState:MJRefreshStateNoMoreData];
+        footer.stateLabel.font = [UIFont systemFontOfSize:17];
+        
+        self.tableView.mj_footer = footer;
+    }
+}
+
+#pragma mark - Load 
+
+/**
+ 自动下拉刷新
+ */
+- (void)autoPullDown {
+    // 马上进入刷新状态
+    [self.tableView.mj_header beginRefreshing];
+}
+
+/**
+ 刷新数据
+ */
+- (void)refreshForNewData {
+    self.pullLoadType = PullDownRefresh;
+    self.page = 1;
+    [self createModel];
+}
+
+/**
+ 下拉刷新
+ */
+- (void)loadNewData {
+    [self refreshForNewData];
+}
+
+/**
+ 上拉更多
+ */
+- (void)loadMoreData {
+    self.pullLoadType = PullUpLoadMore;
+    self.page ++;
+    [self createModel];
+}
+
+/**
+ 结束下拉刷新
+ */
+- (void)endRefresh {
+    if (self.pullLoadType != PullUpLoadMore) {
+        self.pullLoadType = PullDefault;
+    }
+    if (self.tableView.mj_header) {
+        [self.tableView.mj_header endRefreshing];
+    }
+    
+    if (!self.loadMoreEnable) {
+        if (self.tableView.mj_footer) {
+            [self.tableView.mj_footer endRefreshing];
+        }
+    }
+}
+
+/**
+ 结束上拉更多
+ */
+- (void)endLoadMore {
+    self.pullLoadType = PullDefault;
+    if (self.tableView.mj_footer) {
+        if (self.loadMoreEnable) {
+            [self.tableView.mj_footer endRefreshingWithNoMoreData];
+        } else {
+            [self.tableView.mj_footer endRefreshing];
+        }
+    }
+}
+
+/**
+ 结束上下拉状态
+ 带延时动画
+ */
+- (void)endPullStatus {
+    if (self.pullLoadType == PullUpLoadMore) {
+        [self endLoadMore];
+    } else {
+        [self performSelector:@selector(endRefresh) withObject:nil afterDelay:0.62];
+    }
+}
+
+/**
+ 结束上下拉状态 加载失败 to do
+ 带延时动画
+ */
+- (void)endPullStatusFail {
+    [self endPullStatus];
+}
+
+- (void)onDataUpdated {
+    [super onDataUpdated];
+    
+    [self createDataSource];
+    
+    [self handleWhenLessOnePage];
+    [self handleWhenNoneData];
+    
+    [self endPullStatus];
+}
+
+- (void)onLoadFailed {
+    [super onLoadFailed];
+    
+    [self endPullStatusFail];
+}
+
+/**
+ TableView DataSource
+ */
+- (void)createDataSource {
+    /*
+    self.dataSource = [HBTableViewDataSource dataSourceWithItems:self.items];
+     */
+}
+
+/**
+ 处理页面是否能继续上拉更多
+ */
+- (void)handleWhenLessOnePage {
+    /*
+     if (self.items.count < limitSize || self.newItemsCount <= 0) {
+     self.loadMoreEnable = NO;
+     } else{
+     self.loadMoreEnable = NO;
+     }
+     */
+}
+
+/**
+ 展示空页面
+ */
+- (void)handleWhenNoneData {
+    
+}
 
 @end
