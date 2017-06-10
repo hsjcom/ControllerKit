eccd11b150165b3336d3e64049911b9cecb603b2
Shaojie Hong <hongsjcn@qq.com>
2017-06-10 16:04:33 +0800

    SlideBar step 2

--- ControllerKitDemo/ControllerKit/HBSlideBar/HBSlideBar.h
+++ ControllerKitDemo/ControllerKit/HBSlideBar/HBSlideBar.h
@@ -8,6 +8,69 @@
 
 #import <UIKit/UIKit.h>
 
+typedef void(^HBSlideBarItemSelectedCallback)(NSUInteger idx);
+
 @interface HBSlideBar : UIView
+
+@property (copy, nonatomic) NSArray *itemsTitle;
+
+@property (strong, nonatomic) UIColor *itemColor;
+
+@property (strong, nonatomic) UIColor *itemSelectedColor;
+
+@property (strong, nonatomic) UIColor *sliderColor;
+
+@property (strong, nonatomic) UIScrollView *scrollView;
+
+// 是否开启居中样式 default NO 注：内部的item总长度必须小于scrollView的长度才有效
+@property (nonatomic, assign) BOOL middleStyle;
+
+// 每个item固定的宽度，默认是根据字的长度自适应
+@property (nonatomic, assign) CGFloat fixedItemWidth;
+
+// 每个item的间距，default 0
+@property (nonatomic, assign) CGFloat itemPadding;
+
+// 下划线的高度 默认见.m 里面的宏SLIDER_VIEW_HEIGHT
+@property (nonatomic, assign) CGFloat sliderHeight;
+
+// 下划线的固定宽度 默认见.m 里面的宏SLIDER_VIEW_WIDTH，如果要自适应item的宽度（即fixedWidth），该值设为-1即可0为不显示
+@property (nonatomic, assign) CGFloat sliderWidth;
+
+// 标题的字体，默认见FDSlideBarItem里面的宏
+@property (nonatomic, strong) UIFont *titleFont;
+
+@property (nonatomic, strong) UIFont *titleSelectedFont;
+
+// 粗体 default = NO；
+@property (nonatomic, assign) BOOL isBoldFont;
+
+// 关闭下划线滑动动画 , 默认有动画
+@property (nonatomic, assign) BOOL closeAnimation;
+
+// 隐藏下划线 default = NO
+@property (nonatomic, assign) BOOL hideSlideView;
+
+// item 宽度Margin，default 14 * 2.
+@property (nonatomic, assign) CGFloat horizontalMargin;
+
+//item 选中颜色，default clearColor.
+@property (nonatomic, strong) UIColor *itemBgSelectColor;
+
+//item 背景颜色，default clearColor
+@property (nonatomic, strong) UIColor *itemBackgroundColor;
+
+@property (strong, nonatomic) UIView *sliderView;
+
+// Add the callback deal when a slide bar item be selected
+- (void)slideBarItemSelectedCallback:(HBSlideBarItemSelectedCallback)callback;
+
+// Set the slide bar item at index to be selected
+- (void)selectSlideBarItemAtIndex:(NSUInteger)index;
+
+// 没有左右两边的渐变图层
+- (instancetype)initWithFrameWithoutFadeLayer:(CGRect)frame;
+
+- (instancetype)initWithFrame:(CGRect)frame showFadeLayer:(BOOL)showFadeLayer showLiner:(BOOL)showLiner;
 
 @end
--- ControllerKitDemo/ControllerKit/HBSlideBar/HBSlideBar.m
+++ ControllerKitDemo/ControllerKit/HBSlideBar/HBSlideBar.m
@@ -7,15 +7,418 @@
 //
 
 #import "HBSlideBar.h"
+#import "HBSlideBarItem.h"
 
-@implementation HBSlideBar
+#define DEVICE_WIDTH CGRectGetWidth([UIScreen mainScreen].bounds)
+#define DEFAULT_SLIDER_COLOR [UIColor orangeColor]
+#define SLIDER_VIEW_HEIGHT 3
+#define SLIDER_VIEW_WIDTH -1
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+@interface HBSlideBar ()<HBSlideBarItemDelegate>
+
+@property (strong, nonatomic) NSMutableArray *items;
+
+@property (strong, nonatomic) HBSlideBarItem *selectedItem;
+@property (strong, nonatomic) HBSlideBarItemSelectedCallback callback;
+
+@property (nonatomic, weak) UIImageView *leftImgV;
+@property (nonatomic, weak) UIImageView *rightImgV;
+
+@property (nonatomic, assign) CGFloat allWidth;
+
+@end
+
+
+
+
+@implementation HBSlideBar 
+
+#pragma mark - Lifecircle
+
+- (instancetype)init {
+    CGRect frame = CGRectMake(0, 20, DEVICE_WIDTH, 46);
+    return [self initWithFrame:frame];
 }
-*/
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self= [super initWithFrame:frame]) {
+        _items = [NSMutableArray array];
+        UIImageView *lineImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, frame.size.height - 0.5, frame.size.width, 0.5)];
+        lineImageView.backgroundColor = RGBCOLOR(241, 241, 241);
+        [self addSubview:lineImageView];
+        [self initScrollView];
+        [self initLeftRightView];
+        [self initSliderView];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrameWithoutFadeLayer:(CGRect)frame {
+    if (self= [super initWithFrame:frame]) {
+        _items = [NSMutableArray array];
+        UIImageView *lineImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, frame.size.height - 0.5, frame.size.width, 0.5)];
+        lineImageView.backgroundColor = RGBCOLOR(241, 241, 241);
+        [self addSubview:lineImageView];
+        [self initScrollView];
+        [self initSliderView];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame showFadeLayer:(BOOL)showFadeLayer showLiner:(BOOL)showLiner{
+    if (self = [super initWithFrame:frame]) {
+        _items = [NSMutableArray array];
+        if (showLiner) {
+            UIImageView *lineImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, frame.size.height - 0.5, frame.size.width, 0.5)];
+            lineImageView.backgroundColor = RGBCOLOR(241, 241, 241);
+            [self addSubview:lineImageView];
+        }
+        [self initScrollView];
+        [self initSliderView];
+        if (showLiner) {
+            [self initLeftRightView];
+        }
+    }
+    return self;
+}
+
+#pragma - mark Custom Accessors
+
+- (void)setItemsTitle:(NSArray *)itemsTitle {
+    _itemsTitle = itemsTitle;
+    [self setupItems];
+}
+
+- (void)setItemColor:(UIColor *)itemColor {
+    for (HBSlideBarItem *item in _items) {
+        [item setItemTitleColor:itemColor];
+    }
+}
+
+- (void)setItemSelectedColor:(UIColor *)itemSelectedColor {
+    for (HBSlideBarItem *item in _items) {
+        [item setItemSelectedTitleColor:itemSelectedColor];
+    }
+}
+
+- (void)setSliderColor:(UIColor *)sliderColor {
+    _sliderColor = sliderColor;
+    self.sliderView.backgroundColor = _sliderColor;
+}
+
+- (void)setSelectedItem:(HBSlideBarItem *)selectedItem {
+    _selectedItem.selected = NO;
+    _selectedItem = selectedItem;
+}
+
+#pragma - mark Private
+
+- (void)initLeftRightView {
+    UIImageView *leftImgV = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 30, self.height)];
+    leftImgV.image = [[UIImage imageNamed:@"find_slider_left_bg"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0) resizingMode:UIImageResizingModeStretch];
+    [self addSubview:leftImgV];
+    self.leftImgV = leftImgV;
+    
+    UIImageView *rightImgV = [[UIImageView alloc]initWithFrame:CGRectMake(self.width - 30, 0, 30, self.height)];
+    rightImgV.image = [[UIImage imageNamed:@"find_slider_right_bg"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0) resizingMode:UIImageResizingModeStretch];
+    [self addSubview:rightImgV];
+    self.rightImgV = rightImgV;
+}
+
+- (void)initScrollView {
+    _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+    _scrollView.backgroundColor = [UIColor clearColor];
+    _scrollView.showsHorizontalScrollIndicator = NO;
+    _scrollView.showsVerticalScrollIndicator = NO;
+    _scrollView.scrollsToTop = NO;
+    _scrollView.bounces = YES;
+    [self addSubview:_scrollView];
+}
+
+- (void)initSliderView {
+    if (self.hideSlideView) {
+        return;
+    }
+    _sliderView = [[UIView alloc] init];
+    _sliderColor = DEFAULT_SLIDER_COLOR;
+    _sliderView.backgroundColor = _sliderColor;
+    [_scrollView addSubview:_sliderView];
+}
+
+/**
+ * by Soldier
+ * 是否平分tab
+ */
+- (BOOL)isAverage {
+    float itemW = 0;
+    for (NSString *title in _itemsTitle) {
+        if (self.fixedItemWidth > 0 ) {
+            itemW += self.fixedItemWidth;
+        } else {
+            itemW += [HBSlideBarItem widthForTitle:title font:self.titleFont horizontalMargin:(self.horizontalMargin > 0 ? self.horizontalMargin : 0)];
+            if (![title isEqualToString:[_itemsTitle firstObject]] &&
+                ![title isEqualToString:[_itemsTitle lastObject]]) {
+                itemW += self.itemPadding;
+            }
+        }
+    }
+    self.allWidth = itemW;
+    if (_itemsTitle.count > 1 && itemW < _scrollView.width) {
+        if (self.middleStyle) {
+            return  NO;
+        }
+        return YES;
+    } else {
+        self.middleStyle = NO;
+        return NO;
+    }
+}
+
+- (void)setupItems {
+    [_items removeAllObjects];
+    
+    BOOL isAve = [self isAverage];
+    
+    for (UIView *itemView in _scrollView.subviews) {
+        if ([itemView isKindOfClass:[HBSlideBarItem class]]) {
+            HBSlideBarItem *slideBarItem = (HBSlideBarItem *)itemView;
+            slideBarItem.delegate = nil;
+            [slideBarItem removeFromSuperview];
+        }
+    }
+    CGFloat itemX = self.middleStyle ? (_scrollView.width - self.allWidth) /2.0 :0;
+    for (NSString *title in _itemsTitle) {
+        HBSlideBarItem *item = [[HBSlideBarItem alloc] init];
+        item.delegate = self;
+        item.isBoldFont = self.isBoldFont;
+        item.backgroundColor = self.itemBackgroundColor ? self.itemBackgroundColor : [UIColor clearColor];
+        item.itemBackgroundColor = self.itemBackgroundColor;
+        item.itemBgSelectColor = self.itemBgSelectColor;
+        
+        // Init the current item's frame
+        
+        /**
+         * by Soldier
+         */
+        CGFloat itemW  = _scrollView.width * 0.5;
+        if (isAve) {
+            itemW = _scrollView.width / _itemsTitle.count;
+        } else {
+            if (self.fixedItemWidth > 0) {
+                itemW = self.fixedItemWidth;
+            }else {
+                itemW = [HBSlideBarItem widthForTitle:title font:self.titleFont horizontalMargin:(self.horizontalMargin > 0 ? self.horizontalMargin : 0)];
+            }
+        }
+        
+        item.frame = CGRectMake(itemX, 0, itemW, CGRectGetHeight(_scrollView.frame));
+        [item setItemTitle:title];
+        if (self.titleFont) {
+            [item setItemTitleFont:self.titleFont.pointSize];
+        }
+        if (self.titleSelectedFont) {
+            [item setItemSelectedTileFont:self.titleSelectedFont.pointSize];
+        }
+        [_items addObject:item];
+        
+        [_scrollView addSubview:item];
+        
+        // Caculate the origin.x of the next item
+        itemX = CGRectGetMaxX(item.frame) + self.itemPadding;
+    }
+    
+    // Cculate the scrollView 's contentSize by all the items
+    _scrollView.contentSize = CGSizeMake(itemX, CGRectGetHeight(_scrollView.frame));
+    
+    // Set the default selected item, the first item
+    HBSlideBarItem *firstItem = [self.items firstObject];
+    firstItem.selected = YES;
+    _selectedItem = firstItem;
+    self.leftImgV.hidden = YES;
+    
+    if (_sliderView) {
+        // Set the frame of sliderView by the selected item
+        CGFloat sliderWidth = 0;
+        if (self.sliderWidth > 0) {
+            sliderWidth = self.sliderWidth;
+        }else if (self.sliderWidth < 0) {
+            sliderWidth = firstItem.width;
+        }
+        _sliderView.frame = CGRectMake(0, self.frame.size.height - self.sliderHeight, sliderWidth, self.sliderHeight);
+        _sliderView.centerX = firstItem.centerX;
+    }
+}
+
+- (void)scrollToVisibleItem:(HBSlideBarItem *)item {
+    NSInteger selectedItemIndex = [self.items indexOfObject:_selectedItem];
+    NSInteger visibleItemIndex = [self.items indexOfObject:item];
+    
+    // If the selected item is same to the item to be visible, nothing to do
+    if (selectedItemIndex == visibleItemIndex ) {
+        return;
+    }
+    
+    //中间位置的差值
+    CGFloat offsetx = item.center.x - self.scrollView.frame.size.width * 0.5;
+    CGFloat offsetMax = self.scrollView.contentSize.width - self.scrollView.frame.size.width;
+    
+    //中间位置的差值小于0说明这item的中点小于中心点，如果大于测移动相应的距离item居中，但是不超过最大的offsetMax
+    /**
+     * scrollView.contentInset时调整
+     * by Soldier
+     */
+    if (offsetx < 0) {
+        offsetx = self.scrollView.contentInset.left > 0 ? (-self.scrollView.contentInset.left) : 0;
+    } else if (offsetx > offsetMax){
+        offsetx = offsetMax + (self.scrollView.contentInset.right > 0 ? self.scrollView.contentInset.right : 0);
+    }
+    
+    if (self.scrollView.contentSize.width > UI_SCREEN_WIDTH) {
+        [_scrollView setContentOffset:CGPointMake(offsetx, self.scrollView.contentOffset.y) animated:YES];
+    }
+    
+    //    CGPoint offset = _scrollView.contentOffset;
+    
+    // If the item to be visible is in the screen, nothing to do
+    //    if (CGRectGetMinX(item.frame) > offset.x && CGRectGetMaxX(item.frame) < (offset.x + CGRectGetWidth(_scrollView.frame))) {
+    //        return;
+    //    }
+    
+    //    // Update the scrollView's contentOffset according to different situation
+    //    if (selectedItemIndex < visibleItemIndex) {
+    //        // The item to be visible is on the right of the selected item and the selected item is out of screeen by the left, also the opposite case, set the offset respectively
+    //        if (CGRectGetMaxX(_selectedItem.frame) < offset.x) {
+    //            offset.x = CGRectGetMinX(item.frame);
+    //        } else {
+    //            offset.x = CGRectGetMaxX(item.frame) - CGRectGetWidth(_scrollView.frame);
+    //        }
+    //    } else {
+    //        // The item to be visible is on the left of the selected item and the selected item is out of screeen by the right, also the opposite case, set the offset respectively
+    //        if (CGRectGetMinX(_selectedItem.frame) > (offset.x + CGRectGetWidth(_scrollView.frame))) {
+    //            offset.x = CGRectGetMaxX(item.frame) - CGRectGetWidth(_scrollView.frame);
+    //        } else {
+    //            offset.x = CGRectGetMinX(item.frame);
+    //        }
+    //    }
+    //    _scrollView.contentOffset = offset;
+}
+
+- (void)addAnimationWithSelectedItem:(HBSlideBarItem *)item {
+    if (!_sliderView) {
+        return;
+    }
+    
+    // Caculate the distance of translation
+    CGFloat dx = CGRectGetMidX(item.frame) - CGRectGetMidX(_selectedItem.frame);
+    
+    // Add the animation about translation
+    
+    if (!self.closeAnimation) {
+        CABasicAnimation *positionAnimation = [CABasicAnimation animation];
+        positionAnimation.keyPath = @"position.x";
+        positionAnimation.fromValue = @(_sliderView.layer.position.x);
+        positionAnimation.toValue = @(_sliderView.layer.position.x + dx);
+        positionAnimation.duration = 0.2;
+        [_sliderView.layer addAnimation:positionAnimation forKey:@"positionAnimation"];
+    }
+    
+    //    // Add the animation about size
+    //    CABasicAnimation *boundsAnimation = [CABasicAnimation animation];
+    //    boundsAnimation.keyPath = @"bounds.size.width";
+    //    boundsAnimation.fromValue = @(CGRectGetWidth(_sliderView.layer.bounds));
+    //    boundsAnimation.toValue = @(CGRectGetWidth(item.frame));
+    //
+    //    // Combine all the animations to a group
+    //    CAAnimationGroup *animationGroup = [CAAnimationGroup animation];
+    //    animationGroup.animations = @[positionAnimation, boundsAnimation];
+    //    animationGroup.duration = 0.2;
+    //    [_sliderView.layer addAnimation:animationGroup forKey:@"basic"];
+    
+    
+    //     Keep the state after animating
+    _sliderView.layer.position = CGPointMake(_sliderView.layer.position.x + dx, _sliderView.layer.position.y);
+    if (self.sliderWidth < 0) {
+        CGRect rect = _sliderView.layer.bounds;
+        rect.size.width = CGRectGetWidth(item.frame);
+        _sliderView.layer.bounds = rect;
+    }
+}
+
+#pragma mark - Public
+
+- (void)slideBarItemSelectedCallback:(HBSlideBarItemSelectedCallback)callback {
+    _callback = callback;
+}
+
+- (void)selectSlideBarItemAtIndex:(NSUInteger)index {
+    HBSlideBarItem *item = nil;
+    if (index < self.items.count) {
+        item = self.items[index];
+    }
+    
+    if (item == _selectedItem) {
+        return;
+    }
+//    NSLog(@"selectSlideBarItemAtIndex %lu",(unsigned long)index);
+    item.selected = YES;
+    
+    [self scrollToVisibleItem:item];
+    [self addAnimationWithSelectedItem:item];
+    
+    self.selectedItem = item;
+    
+    if (index == 0) {
+        self.leftImgV.hidden = YES;
+        self.rightImgV.hidden = NO;
+    } else if (index == self.items.count - 1) {
+        self.leftImgV.hidden = NO;
+        self.rightImgV.hidden = YES;
+    } else {
+        self.leftImgV.hidden = NO;
+        self.rightImgV.hidden = NO;
+    }
+}
+
+- (CGFloat)sliderHeight {
+    if (_sliderHeight == 0) {
+        _sliderHeight = SLIDER_VIEW_HEIGHT;
+    }
+    return _sliderHeight;
+}
+
+- (CGFloat)sliderWidth {
+    if (_sliderWidth == 0) {
+        _sliderWidth = SLIDER_VIEW_WIDTH;
+    }
+    return _sliderWidth;
+}
+
+#pragma mark - HBSlideBarItemDelegate
+
+- (void)slideBarItemSelected:(HBSlideBarItem *)item {
+    if (item == _selectedItem) {
+        return;
+    }
+    
+    [self addAnimationWithSelectedItem:item];
+    [self scrollToVisibleItem:item];
+    
+    self.selectedItem = item;
+    
+    NSInteger index = [self.items indexOfObject:item];
+    if (index == 0) {
+        self.leftImgV.hidden = YES;
+        self.rightImgV.hidden = NO;
+    } else if(index == self.items.count - 1) {
+        self.leftImgV.hidden = NO;
+        self.rightImgV.hidden = YES;
+    } else {
+        self.leftImgV.hidden = NO;
+        self.rightImgV.hidden = NO;
+    }
+    
+    _callback([self.items indexOfObject:item]);
+}
+
 
 @end
